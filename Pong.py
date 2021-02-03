2d38f88bd1779156df8903be68aa77fe652300b8
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2021-02-04 00:36:04 +0530

    change is so long that i forgot

--- Pong.py
+++ Pong.py
@@ -5,9 +5,9 @@ import os
 pg.init()
 
 width = 800
-height = 600
+height = 500
 
-screen = pg.display.set_mode((width, height+100))
+screen = pg.display.set_mode((width, height+150))
 pg.display.set_caption("By - Aryan")
 
 icon = pg.image.load(os.path.dirname(os.path.realpath(__file__)) + '/pong.png')
@@ -15,9 +15,9 @@ pg.display.set_icon(icon)
 clock = pg.time.Clock()
 font_style = pg.font.SysFont("bahnschrift", 25)
 score_font = pg.font.SysFont("comicsansms", 35)
-def message(msg,color):
+def message(msg,color,height,width):
     info = font_style.render(msg, True, color)
-    screen.blit(info, [width/3, height+60])
+    screen.blit(info, [width, height+60])
 
 boxSize = 10
 panelLength = 70
@@ -39,6 +39,8 @@ y_change = 0
 score1 = 0
 score2 = 0
 
+allowMove = 1
+
 direction_x = 1
 direction_y = 1
 
@@ -55,15 +57,6 @@ while running:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_q:
                 running = False
-    keys=pg.key.get_pressed()
-    if keys[pg.K_s] and y2<height-70:
-        y2+=20
-    if keys[pg.K_w] and y2>0:
-        y2-=20
-    if keys[pg.K_UP] and y1>0:
-        y1-=20
-    if keys[pg.K_DOWN] and y1<height-70:
-        y1+=20
     x_change = boxSize
     y_change = boxSize
     if x_cod >= width:
@@ -74,21 +67,35 @@ while running:
         score2+=1
     if x_cod == x2+10 and y_cod>=y2 and y_cod<=y2+70:
         direction_x=-direction_x
+        allowMove=0
     if x_cod == x1 and y_cod>=y1 and y_cod<=y1+70:
         direction_x=-direction_x
+        allowMove=0
     if y_cod >= height or y_cod<0:
         direction_y=-direction_y
-            
+        allowMove=1
+        
     x_cod -= x_change*direction_x
     y_cod -= y_change*direction_y
+    keys=pg.key.get_pressed()
+    if keys[pg.K_s] and y2<height-70 and allowMove==1:
+        y2+=20
+    if keys[pg.K_w] and y2>0 and allowMove==1:
+        y2-=20
+    if keys[pg.K_UP] and y1>0 and allowMove==1:
+        y1-=20
+    if keys[pg.K_DOWN] and y1<height-70 and allowMove==1:
+        y1+=20
+    
 
-    screen.fill((120,120,120))
+    screen.fill((0,0,0))
     pg.draw.circle(screen,(255,255,0),[x_cod,y_cod],10)
     pg.draw.rect(screen,(255,255,255),[x1,y1,boxSize,panelLength])
     pg.draw.rect(screen,(255,255,255),[x2,y2,boxSize,panelLength])
     pg.draw.line(screen,(0,0,0),(0,height+10),(width,height+10))
     Your_score(score1,score2)
-    message("Press q to quit game", (255,0,0))
+    message("Press q to quit game", (255,0,0),height,width/3)
+    message("Paddle will move only after it bounces back from top or bottom", (255,0,0),height+30,20)
     pg.display.update()
     clock.tick(15)
 
